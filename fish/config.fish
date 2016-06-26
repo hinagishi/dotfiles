8c5bd44cf67f6f07cdc1fea23ef619708f868b72
hinagishi <archangel.ratziel@gmail.com>
2016-06-27 00:31:27 +0900

    add fish config

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+fish_vi_key_bindings
+
+set -x GOROOT /usr/local/go
+set -x GOPATH $HOME/repos
+set -x PATH $PATH $GOPATH/bin
+
+function fish_user_key_bindings
+    bind \cn peco_src
+    bind \cr peco_history
+end
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,5 @@
+function fish_prompt
+    set_color magenta
+    echo -n "(*っ'-')っ "
+    set_color normal
+end
