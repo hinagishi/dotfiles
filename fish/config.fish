68fb63eb984f1067474bcc1622ba68151741c924
hinagishi <archangel.ratziel@gmail.com>
2016-06-29 13:24:42 +0900

    modify

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,20 @@
-init_powerline
+if test (uname) = "Darwin"
+    if test -e $HOME/Library/Python/2.7/lib/python/site-packages/powerline/bindings/tmux
+        set -x POWERLINE $HOME/Library/Python/2.7/lib/python/site-packages/powerline/bindings/tmux
+        set -x PATH $PATH $HOME/Library/Python/2.7/bin
+    else if test -e $HOME/Library/Python/3.5/lib/python/site-packages/powerline/bindings/tmux
+        set -x POWERLINE $HOME/Library/Python/3.5/lib/python/site-packages/powerline/bindings/tmux
+        set -x PATH $PATH $HOME/Library/Python/3.5/bin
+    end
+else if test (uname) = "Linux"
+    echo "linux"
+    if test -e $HOME/.local/lib/python2.7/site-packages/powerline/bindings/tmux
+        echo "set val"
+        set -x POWERLINE $HOME/.local/lib/python2.7/site-packages/powerline/bindings/tmux
+        set -x PATH $PATH $HOME/.local/bin
+    end
+end
+
 if not test $TMUX
     tmux -2
 end
--- fish/functions/init_powerline.fish
+++ fish/functions/init_powerline.fish
@@ -8,7 +8,9 @@ function init_powerline
             set -x PATH $PATH $HOME/Library/Python/3.5/bin
         end
     else if test (uname) = "Linux"
+        echo "linux"
         if test -e $HOME/.local/lib/python2.7/site-packages/powerline/bindings/tmux
+            echo "set val"
             set -x POWERLINE $HOME/.local/lib/python2.7/site-packages/powerline/bindings/tmux
             set -x PATH $PATH $HOME/.local/bin
         end
